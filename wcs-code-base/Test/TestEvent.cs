cb97df319596b4d4f4b4f6401a8a879e832bdd27
PREVOST Corentin <prevost.corentin@yahoo.com>
2020-03-05 17:35:20 +0100

    remove tests

--- wcs-code-base/Test/TestEvent.cs
+++ wcs-code-base/Test/TestEvent.cs
@@ -15,45 +15,5 @@ namespace WCSTest
 			Assert.AreEqual(startDateBeforePostpone, presentEvent.StartDate - TimeSpan.FromDays(1));
 			Assert.AreEqual(endDateBeforePostpone, presentEvent.EndDate - TimeSpan.FromDays(1));
 		}
-
-		[Test]
-		public void TestPresentEvent()
-		{
-			Event presentEvent = ApiTest.CreateDelayedEvent();
-
-			Assert.IsTrue(presentEvent.IsPresent);
-		}
-
-		[Test]
-		public void TestPresentEventIsNotPassed()
-		{
-			Event presentEvent = CreateDelayedEvent();
-
-			Assert.IsFalse(happeningEvent.IsPassed);
-		}
-
-		[Test]
-		public void TestEventIsPassed()
-		{
-			Event pastEvent = ApiTest.CreateDelayedEvent(TimeSpan.FromHours(-1));
-
-			Assert.IsTrue(pastEvent.IsPassed);
-		}
-
-		[Test]
-		public void TestEventIsFuture()
-		{
-			Event futureEvent = ApiTest.CreateDelayedEvent(TimeSpan.FromHours(1));
-
-			Assert.IsTrue(futureEvent.IsFuture);
-		}
-
-		[Test]
-		public void TestFutureIsNotPresent()
-		{
-			Event futureEvent = CreateFutureEvent(TimeSpan.FromHours(1));
-
-			Assert.IsFalse(futureEvent.IsPresent);
-		}
 	}
 }
